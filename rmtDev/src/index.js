ceb1dfb7dcafe4f62abdf43c0b39ca888f200232
Farelart <ffjdganlaky@gmail.com>
2024-10-26 00:26:55 +0100

    final making it ready for webpack

---
+++ rmtDev/src/index.js
@@ -0,0 +1,8 @@
+import "./components/Error.js";
+import "./components/Search.js";
+import "./components/JobList.js";
+import "./components/Spinner.js";
+import "./components/JobDetails.js";
+import "./components/Sorting.js";
+import "./components/Pagination.js";
+import "./components/Bookmarks.js";
